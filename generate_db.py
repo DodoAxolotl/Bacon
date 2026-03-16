32c0b4aef00d0a448d3baa28977d60f6d659c65d
dor <dor.m.berko@gmail.com>
2026-03-16 12:44:23 +0200

    added generate_db

---
+++ generate_db.py
@@ -0,0 +1,55 @@
+import csv
+import json
+from typing import Dict, Tuple, List
+
+
+def generate_db() -> None:
+    movie_names = get_all_movies()
+    movies, actors = get_all_roles(movie_names)
+    get_all_actors(movies, actors)
+    db = {"movies": movies, "actors": actors}
+    with open("db.json", "w") as file:
+        json.dump(db, file)
+
+
+def get_all_actors(movies: Dict[str, List[str]], actors: Dict[str, List[str]]) -> None:
+    with open("name.basics.tsv", newline="", encoding="utf-8") as file:
+        content = csv.DictReader(file, delimiter="\t")
+        for row in content:
+            if row["nconst"] in actors:
+                actor, name = row["nconst"], row["primaryName"]
+                actors[name] = actors.pop(actor)
+                for movie in actors[name]:
+                    movies[movie].remove(actor)
+                    movies[movie].append(name)
+
+
+def get_all_roles(
+    movie_names: Dict[str, str],
+) -> Tuple[Dict[str, List[str]], Dict[str, List[str]]]:
+    movies = {}
+    actors = {}
+    with open("title.principals.tsv", newline="", encoding="utf-8") as file:
+        content = csv.DictReader(file, delimiter="\t")
+        for row in content:
+            if row["tconst"] in movie_names and row["characters"] != "\\\\N":
+                movie, actor = movie_names[row["tconst"]], row["nconst"]
+                movies.setdefault(movie, [])
+                movies[movie].append(actor)
+                actors.setdefault(actor, [])
+                actors[actor].append(movie)
+    return movies, actors
+
+
+def get_all_movies() -> Dict[str, str]:
+    movie_names = {}
+    with open("title.basics.tsv", newline="", encoding="utf-8") as file:
+        content = csv.DictReader(file, delimiter="\t")
+        for row in content:
+            if row["titleType"] == "movie" and row["startYear"] == "1978":
+                movie_names[row["tconst"]] = row["primaryTitle"]
+    return movie_names
+
+
+if __name__ == "__main__":
+    generate_db()
