b8c2580649ae088a68a1ef660dda4d79141daa1a
dor <dor.m.berko@gmail.com>
2026-03-16 15:03:50 +0200

    added bacon_distance

---
+++ bacon_distance.py
@@ -0,0 +1,42 @@
+import json
+import math
+import sys
+
+
+def calculate_bacon_distance(name: str) -> float:
+    with open("db.json") as file:
+        db = json.load(file)
+    if name not in db["actors"]:
+        raise ValueError(f"The specified actor does not exist in our database")
+    distance = 0
+    seen_movies = set()
+    seen_actors = set()
+    current_movies = set()
+    current_actors = set(["Kevin Bacon"])
+    while name not in current_actors:
+        if len(current_actors) == 0:
+            return math.inf
+        current_movies = set(
+            [
+                movie
+                for actor in current_actors
+                for movie in db["actors"][actor]
+                if movie not in seen_movies
+            ]
+        )
+        current_actors = set(
+            [
+                actor
+                for movie in current_movies
+                for actor in db["movies"][movie]
+                if actor not in seen_actors
+            ]
+        )
+        seen_movies.update(current_movies)
+        seen_actors.update(current_actors)
+        distance += 1
+    return distance
+
+
+if __name__ == "__main__":
+    print(calculate_bacon_distance(sys.argv[1]))
